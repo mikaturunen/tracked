438a2d6757d907174772ab497b02302766dfd581
Mika Turunen <mika.turunen@ymail.com>
2018-03-17 15:52:50 +0200

    #2 add redux and redux types. Use simple stupid example.

--- source/frontend/source/components/Hello.tsx
+++ source/frontend/source/components/Hello.tsx
@@ -3,10 +3,11 @@ import * as React from 'react';
 export interface HelloProps {
     compiler: string;
     framework: string;
+    value: number;
 }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello, World! from React Hello.tsx.</h1>
+        return <h1>Hello, World! from React Hello.tsx. this {this.props.value}</h1>
     }
 }
--- source/frontend/source/index.tsx
+++ source/frontend/source/index.tsx
@@ -1,9 +1,21 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { createStore} from 'redux';
 
-import { Hello } from "./components/Hello";
+import Reducers from './reducers/index'
+import { Hello } from './components/Hello';
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+const store = createStore(Reducers);
+const rootElement = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+    <Hello
+        compiler='TypeScript'
+        framework='React'
+        value={store.getState()}
+    />,
+    document.getElementById('example')
 );
+
+render();
+store.subscribe(render);
